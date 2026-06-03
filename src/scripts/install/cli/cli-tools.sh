5cc3343c8ee34fec9451ccbb0b5b8aba175b08ec
Garret Patten <garret.patten@proton.me>
2026-06-03 13:17:29 -0400

    Add lazygit installation

--- src/scripts/install/cli/cli-tools.sh
+++ src/scripts/install/cli/cli-tools.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y bat curl eza fd-find git htop jq ripgrep vim wget
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y bat curl eza fd-find git htop jq lazygit ripgrep vim wget
